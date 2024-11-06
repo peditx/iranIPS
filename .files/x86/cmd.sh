7f18d0bcfc3219022555e0313877a29b9b85cceb
PeDitX <pedram.ale@me.com>
2024-11-06 21:42:56 +0330

    Update cmd.sh

--- .files/x86/cmd.sh
+++ .files/x86/cmd.sh
@@ -12,13 +12,13 @@ NC='\033[0m' # No Color
 
 # Check for root privileges
 if [[ $EUID -ne 0 ]]; then
-    echo -e "${RED}This script must be run as root. Please use 'sudo'.${NC}"
+    echo "${RED}This script must be run as root. Please use 'sudo'.${NC}"
     exit 1
 fi
 
 # Check if wget is installed
 if ! command -v wget &> /dev/null; then
-    echo -e "${RED}wget is not installed. Installing it now...${NC}"
+    echo "${RED}wget is not installed. Installing it now...${NC}"
     apt update && apt install -y wget
 fi
 
@@ -29,7 +29,7 @@ wget https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/op/resize.
 clear
 
 # Display the banner in magenta
-echo -e "${MAGENTA}
+echo "${MAGENTA}
  _______           _______  __   __     __    __            __          
 |       \         |       \|  \ |  \   |  \  |  \          |  \         
 | ▓▓▓▓▓▓▓\ ______ | ▓▓▓▓▓▓▓\\▓▓_| ▓▓_  | ▓▓  | ▓▓ ______  _| ▓▓_        
@@ -44,28 +44,28 @@ echo -e "${MAGENTA}
 ${NC}"
 
 # Welcome message
-echo -e "${GREEN}Welcome to the installer!${NC}"
+echo "${GREEN}Welcome to the installer!${NC}"
 echo ""
 
 # Check the system OS
 OS_NAME=$(uname -o)
 
 # Show warning message if the OS is OpenWRT or ImmortalWRT
-echo -e "${RED} If your operating system is OpenWRT or ImmortalWRT, this section may not function properly and could potentially harm your device. It is advisable to choose option 0 to return to the main menu. ${NC}"
+echo "${RED}If your operating system is OpenWRT or ImmortalWRT, this section may not function properly and could potentially harm your device. It is advisable to choose option 0 to return to the main menu.${NC}"
 echo ""
 
 # Prompt user to continue
-echo -e "Press Enter to continue"
+echo "Press Enter to continue"
 read -r
 
 # Show options in yellow
-echo -e "${YELLOW}Please select your OS you need to install:${NC}"
-echo -e "${RED}1-${NC} ${MAGENTA}PeDitXrt${NC}"
-echo -e "${RED}2-${NC} ${CYAN}MikroTik${NC}"
-echo -e "${RED}3-${NC} ${GREEN}OpenWRT${NC}"
-echo -e "${RED}4-${NC} ${YELLOW}ImmortalWRT${NC}"
-echo -e "${RED}5-${NC} ${BLUE}Custom Link${NC}"
-echo -e "${RED}0-${NC} Back to main Menu${NC}"
+echo "${YELLOW}Please select your OS you need to install:${NC}"
+echo "${RED}1-${NC} ${MAGENTA}PeDitXrt${NC}"
+echo "${RED}2-${NC} ${CYAN}MikroTik${NC}"
+echo "${RED}3-${NC} ${GREEN}OpenWRT${NC}"
+echo "${RED}4-${NC} ${YELLOW}ImmortalWRT${NC}"
+echo "${RED}5-${NC} ${BLUE}Custom Link${NC}"
+echo "${RED}0-${NC} Back to main Menu${NC}"
 echo ""
 
 # Loop until a valid choice is made
@@ -75,7 +75,7 @@ while true; do
     # Check if the choice is valid and handle the corresponding action
     case "$choice" in
         0)
-            echo -e "${CYAN}Running cleanup and downloading ezp.sh...${NC}"
+            echo "${CYAN}Running cleanup and downloading ezp.sh...${NC}"
             rm -f ezp.sh && wget https://github.com/peditx/EZpasswall/raw/refs/heads/main/ezp.sh && chmod +x ezp.sh && ./ezp.sh
             break
             ;;
@@ -92,7 +92,7 @@ while true; do
             script_name=$(echo $script_info | cut -d ' ' -f 1)
             script_url=$(echo $script_info | cut -d ' ' -f 2)
 
-            echo -e "${CYAN}Downloading $script_name from $script_url...${NC}"
+            echo "${CYAN}Downloading $script_name from $script_url...${NC}"
 
             # Download the script using wget
             wget "$script_url" -O "$script_name"
@@ -102,12 +102,12 @@ while true; do
                 chmod +x "$script_name"
                 ./"$script_name"
             else
-                echo -e "${RED}Error: $script_name not found after download.${NC}"
+                echo "${RED}Error: $script_name not found after download.${NC}"
             fi
             break
             ;;
         *)
-            echo -e "${MAGENTA}Invalid choice. Please enter a number between 0 and 5.${NC}"
+            echo "${MAGENTA}Invalid choice. Please enter a number between 0 and 5.${NC}"
             ;;
     esac
 done
