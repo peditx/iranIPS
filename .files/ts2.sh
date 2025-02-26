d1480c6683c2fffb11dda50f0a6d4b2c9e13ba3b
PeDitX <pedram.ale@me.com>
2025-02-26 10:38:50 +0330

    Update and rename ts.sh to ts2.sh

--- .files/ts2.sh
+++ .files/ts2.sh
@@ -6,6 +6,12 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+# Create /var/lock directory if it doesn't exist
+if [ ! -d "/var/lock" ]; then
+    echo "Creating /var/lock directory..."
+    mkdir -p /var/lock
+fi
+
 # Fetch latest release version from GitHub API
 latest_version=$(curl -s https://api.github.com/repos/peditx/luci-app-themeswitch/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
